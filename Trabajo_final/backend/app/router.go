edee5cf652e32a7abdda9bd0edd543b3ec4d066f
simonbf15 <barralesimon@gmail.com>
2024-05-02 19:57:58 -0300

    primer avance, funcion login

--- Trabajo_final/backend/app/router.go
+++ Trabajo_final/backend/app/router.go
@@ -1,1 +1,11 @@
 package app
+
+import (
+	"backend/controllers/users"
+
+	"github.com/gin-gonic-gin"
+)
+
+func MapRoutes(engines *gin.Engine) {
+	engine.POST("/users/login", users.Login)
+}
--- Trabajo_final/backend/services/users/users_service.go
+++ Trabajo_final/backend/services/users/users_service.go
@@ -1,1 +1,9 @@
 package users
+
+import "backend/domain/users"
+
+func Login(user string, password string) users.LoginResponse {
+	return users.LoginResponse{
+		Token: "contraseña123",
+	}
+}
