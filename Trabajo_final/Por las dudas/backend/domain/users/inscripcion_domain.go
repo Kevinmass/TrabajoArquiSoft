566d5379ca48e524a3ec65db51dca0265aca67eb
Kevinmass <michilover@duck.com>
2024-06-13 11:43:12 -0300

    Se borro la carpeta por las dudas

--- Trabajo_final/Por las dudas/backend/domain/users/inscripcion_domain.go
+++
@@ -1,9 +0,0 @@
-package users
-
-type InscriptosData struct {
-	ID        uint `gorm:"primary_key;auto_increment"`
-	ClienteID uint `gorm:"not null"`
-	CursoID   uint `gorm:"not null"`
-}
-
-type Inscriptos []InscriptosData
--- Trabajo_final/Por las dudas/backend/dto/dto_inscripcion.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type InscriptosDataDto struct {
-	ClienteID uint `json:"Client_id"`
-	CursoID   uint `json:"Curso_id"`
-}
