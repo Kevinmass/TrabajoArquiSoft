cc62e94e122e4fd417e2c506c25ec7910edd6e4e
Rous444 <118020966+Rous444@users.noreply.github.com>
2024-06-12 23:41:48 -0300

    ultimos cambios frontend

--- Trabajo_final/frontend/app1/src/componentes/buscador.js
+++ Trabajo_final/frontend/app1/src/componentes/buscador.js
@@ -1,85 +1,83 @@
-import { useState, useEffect, Component } from "react"
-import "./estilos.css"
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
+import "./estilos.css";
+import { Link } from "react-router-dom";
+
 export const Buscador = () => {
-
-  const [query, setQuery] = useState("")
-  const [cursos, setCursos] = useState([])
-  const [resultados, setResultados] = useState(false)
-
+  const [query, setQuery] = useState("");
+  const [cursos, setCursos] = useState([]);
+  const [resultados, setResultados] = useState(false);
 
   const MostrarCursos = () => {
-
-    fetch('http://localhost:8080/cursos')
-      .then(response => response.json())
-      .then(data => {
+    fetch("http://localhost:8080/cursos")
+      .then((response) => response.json())
+      .then((data) => {
         setCursos(data);
       })
-      .catch(error => console.error('error al cargar los cursos', error))
-
-  }
+      .catch((error) => console.error("error al cargar los cursos", error));
+  };
 
   useEffect(() => {
-
     MostrarCursos();
-  }, [])
+  }, []);
 
   const LimpiarBusqueda = () => {
     setResultados(false);
     MostrarCursos();
     setQuery("");
-
-  }
+  };
 
   const inscribirCurso = async (datos) => {
     try {
-      const response = await fetch('http://localhost:8080/inscribirse', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8080/inscribirse", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(datos)
+        body: JSON.stringify(datos),
       });
       if (!response.ok) {
-        throw new Error('error al inscribirse')
+        throw new Error("error al inscribirse");
       }
       const data = await response.json();
-      alert('inscripción realizada correctamente!')
+      alert("inscripción realizada correctamente!");
+    } catch {
+      alert("ya estás inscripto a este curso!");
     }
-    catch {
-      alert('ya estás inscripto a este curso!')
-    }
-  }
+  };
+
   const cargarDatos = (e) => {
-    setQuery(e.target.value)
-  }
-
-
+    setQuery(e.target.value);
+  };
 
   const Buscar = () => {
-    fetch('http://localhost:8080/cursosPorNombre/' + query)
-      .then(response => response.json())
-      .then(data => {
-        setCursos(Array.isArray(data) ? data : [data]);  //con esta linea aseguramos que el curso esté dentro de un array y no tire el error de cursos.map is not a function
+    fetch("http://localhost:8080/cursosPorNombre/" + query)
+      .then((response) => response.json())
+      .then((data) => {
+        setCursos(Array.isArray(data) ? data : [data]); // Asegura que el curso esté dentro de un array
         setResultados(data.length === 0);
       })
-      .catch(error => alert('por favor introduce el nombre del curso deseado', error))
-  }
-
+      .catch((error) =>
+        alert("por favor introduce el nombre del curso deseado", error)
+      );
+  };
 
   return (
     <div className="App">
       <div className="caja-buscador">
-        <div className='buscador'>
+        <div className="buscador">
           <input
-            type='text'
+            type="text"
             value={query}
-            placeholder='Qué curso estás buscando?'
+            placeholder="Qué curso estás buscando?"
             onChange={cargarDatos}
           />
+        </div>
+        <div className="botones-buscador">
           <button onClick={Buscar}>Buscar</button>
-          <button onClick={LimpiarBusqueda}>Limpiar busqueda</button>
-          <button><Link to="/miscursos">Mis cursos</Link></button>
+          <button onClick={LimpiarBusqueda}>Limpiar búsqueda</button>
+          <Link to="/miscursos" className="link-button">
+            Mis cursos
+          </Link>
         </div>
       </div>
       <div className="muestraCursos">
@@ -90,24 +88,28 @@ export const Buscador = () => {
             <div className="cursoIndividual" key={curso.id}>
               <h2>{curso.nombre}</h2>
               <p>Descripción: {curso.descripcion}</p>
-              <p>Profesor: {curso.profesor_nombre} {curso.profesor_apellido}</p>
+              <p>
+                Profesor: {curso.profesor_nombre} {curso.profesor_apellido}
+              </p>
               <p>Correo del profesor: {curso.profesor_correo}</p>
               <p>Fecha de Creación: {curso.fecha_creacion}</p>
               <p>Fecha de Modificación: {curso.fecha_actualizacion}</p>
-              <button onClick={() => {
-                var datos = {
-                  User: localStorage.getItem('user'),
-                  Curso_id: curso.id
-                };
-                console.log(datos)
-                inscribirCurso(datos);
-              }}>Inscribirse al curso</button>
+              <button
+                onClick={() => {
+                  var datos = {
+                    User: localStorage.getItem("user"),
+                    Curso_id: curso.id,
+                  };
+                  console.log(datos);
+                  inscribirCurso(datos);
+                }}
+              >
+                Inscribirse al curso
+              </button>
             </div>
-          )
-          )
+          ))
         )}
       </div>
     </div>
-
   );
-}
+};
