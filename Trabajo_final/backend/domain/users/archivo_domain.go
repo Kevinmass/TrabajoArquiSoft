892d3616a4aa8e761082512e9e402c60892e6d2d
Kevinmass <michilover@duck.com>
2024-07-17 08:56:11 -0300

    cambio en archivos de mediumtext a longtext

--- Trabajo_final/backend/domain/users/archivo_domain.go
+++ Trabajo_final/backend/domain/users/archivo_domain.go
@@ -2,7 +2,7 @@ package users
 
 type Archivodata struct {
 	ID      uint   `gorm:"primary_key;auto_increment"`
-	Base64  string `gorm:"type:mediumtext"`
+	Base64  string `gorm:"type:longtext"`
 	CursoId string `gorm:"type:varchar(100)"`
 }
 type Archivo []Archivodata
